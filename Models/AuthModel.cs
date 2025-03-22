c602a6cd394393abf9f71411911224d291a65f51
mohamed-hafis <132044794+mohamed-hafis@users.noreply.github.com>
2025-03-22 13:06:26 +0530

    Update Auth.cs

--- Models/AuthModel.cs
+++ Models/AuthModel.cs
@@ -1,16 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MyApi.Models
 {
-    public class AuthModel
+    public class Auth
     {
         public class LoginRequest
         {
             public string UserName { get; set; }
             public string Password { get; set; }
+
         }
 
         public class SignupRequest
@@ -18,6 +19,8 @@ namespace MyApi.Models
             public string UserName { get; set; }
             public string Password { get; set; }
             public string ConfirmPassword { get; set; }
+            public string Role { get; set; }
+
 
         }
     }
